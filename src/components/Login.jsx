796f3c462c35fb7849cb8b61bc4e74a5d43a7af6
Rahul <rahulpratik1212@gmail.com>
2024-10-02 02:28:03 +0530

    add login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useRef } from "react";
 
 const Login = () => {
-  return (
-    <div className='h-[80vh]'>Login</div>
-  )
-}
+  const userEmail = useRef();
+  const userPassword = useRef();
 
-export default Login
+  return (
+    <div className="h-[90vh] flex items-center justify-center max-lg:w-full">
+      <div className="card bg-[#fde825] text-neutral-content w-96 rounded-lg">
+        <div className="card-body items-center text-center">
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text ">Enter Your Email</span>
+            </div>
+            <input
+              value={userEmail.current}
+              onChange={(e) => {
+                userEmail.current = e.target.value;
+              }}
+              type="text"
+              placeholder="Eamil@gmail.com"
+              className="input input-bordered w-full max-w-xs rounded-md text-black"
+            />
+          </label>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text ">Enter Your Password</span>
+            </div>
+            <input
+              value={userPassword.current}
+              onChange={(e) => {
+                userPassword.current = e.target.value;
+              }}
+              type="password"
+              placeholder="Password@123"
+              className="input input-bordered w-full max-w-xs rounded-md text-black"
+            />
+          </label>
+          <div className="card-actions justify-end mt-[2vh]">
+            <button className="btn btn-warning rounded-md px-[2vw]">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
